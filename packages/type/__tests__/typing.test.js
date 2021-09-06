48b81406d9f55c57797abed45fe5fa1e8d4c3485
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-06 21:00:24 +0800

    add: 添加单元测试用例

--- packages/type/__tests__/typing.test.js
+++ packages/type/__tests__/typing.test.js
@@ -2,8 +2,25 @@
 
 const type = require('..');
 
-console.log(type.of([]));
+
 
 describe('@trz/type', () => {
-    it('needs tests');
+    test('Function: type.of', () => {
+        expect(type.of([])).toBe('array');
+
+        expect(type.of('this is a string')).toBe('string');
+
+        expect(type.of(new Map)).toBe('map');
+    });
+
+    test('Function: type.is', () => {
+        expect(type.is([], 'array')).toBe(true);
+        expect(type.is([], 'object')).toBe(false);
+
+        expect(type.is({}, 'object')).toBe(true);
+        expect(type.is(new Object(), 'array')).toBe(false);
+
+        expect(type.is('123456789', 'string')).toBe(true);
+        expect(type.is('123456789', null)).toBe(false);
+    });
 });
