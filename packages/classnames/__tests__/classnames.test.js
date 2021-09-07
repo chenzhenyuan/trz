dff21a4f73dc68e965b9c95ba4fcda540e6592fb
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-07 13:41:02 +0800

    upd: 添加`@trz/classnames`单元测试

--- packages/classnames/__tests__/classnames.test.js
+++ packages/classnames/__tests__/classnames.test.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const classnames = require('..');
+const classnames = require('..').default;
 
 describe('@trz/classnames', () => {
-    it('needs tests');
+    test('Function: classnames', () => {
+        expect(classnames('cls1')).toBe('cls1');
+
+        expect(classnames('cls1', 'cls2')).toBe('cls1 cls2');
+
+        expect(classnames('cls1', {"cls2": true})).toBe('cls1 cls2');
+
+        expect(classnames('cls1', {"cls2": false, "cls3": true})).toBe('cls1 cls3');
+
+        expect(classnames('cls1', ["cls2", "cls3"])).toBe('cls1 cls2 cls3');
+
+        expect(classnames('cls1', ["cls2", "cls3"], {"cls4": true})).toBe('cls1 cls2 cls3 cls4');
+
+        expect(classnames('cls1', ["cls2", {"cls3": true}], {"cls4": true})).toBe('cls1 cls2 cls3 cls4');
+
+        expect(classnames('cls1', ["cls2", {"cls3": true}], {"cls4": true})).toBe('cls1 cls2 cls3 cls4');
+
+        expect(classnames('cls1', ["cls2", {"cls3": true, "cls2": true}, "cls1"], {"cls4": true, "cls3": true})).toBe('cls1 cls2 cls3 cls4');
+    });
 });
