6116987c28d93e4597cce382af0cad2c161760fd
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-18 19:48:57 +0800

    修正一处正则错误

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -1,16 +1,16 @@
 
-
 class Url {
-  constructor(urlString) {
-    const rUrl = /((?:^((?:[a-z]+)\:)?\/\/)?(((?:\.?[a-z\-0-9]+)+)(?:\:(\d{1,5}))?))((?:\/+[^\/\?\#]+)+\/{0,})(\?[^#]{0,})?(\#[^#]{0,})$/gi;
+  constructor(urlString: string) {
+    const rUrl = /((?:^((?:[a-z]+)\:)?\/\/)?(((?:\.?[a-z\-0-9]+)+)(?:\:(\d{1,5}))?))((?:\/+[^\/\?\#]+)+\/{0,})(\?[^#]{0,})?(\#[^#]{0,})?$/gi;
     const [ href, origin, protocol, host, hostname, port, pathname, search, hash ] = rUrl.exec(urlString) || [];
     const urlObj = { href, origin, protocol, host, hostname, port, pathname, search, hash };
 
     Object.keys((urlObj)).forEach((name) => {
-      this[ name ] = urlObj[ name ];
+      this[ name ] = urlObj[ name ] || '';
     })
+
   }
 }
 
-
 export default Url;
+
