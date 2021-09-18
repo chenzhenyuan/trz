528b579f71bb2b77982a2014a82a5f67de854bed
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-18 19:18:40 +0800

    添加: Url 主要功能类

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -1,7 +1,16 @@
-'use strict';
 
-module.exports = url;
 
-function url() {
-    // TODO
+class Url {
+  constructor(urlString) {
+    const rUrl = /((?:^((?:[a-z]+)\:)?\/\/)?(((?:\.?[a-z\-0-9]+)+)(?:\:(\d{1,5}))?))((?:\/+[^\/\?\#]+)+\/{0,})(\?[^#]{0,})?(\#[^#]{0,})$/gi;
+    const [ href, oringin, protocol, host, hostname, port, pathname, search, hash ] = rUrl.exec(urlString) || [];
+    const urlObj = { href, oringin, protocol, host, hostname, port, pathname, search, hash };
+
+    Object.keys((urlObj)).forEach((name) => {
+      this[ name ] = urlObj[ name ];
+    })
+  }
 }
+
+
+export default Url;
