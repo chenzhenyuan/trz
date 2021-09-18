e2faededa7b787738d0ba357183195512bbaa520
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-18 19:21:12 +0800

    修正:一处拼写错误

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -3,8 +3,8 @@
 class Url {
   constructor(urlString) {
     const rUrl = /((?:^((?:[a-z]+)\:)?\/\/)?(((?:\.?[a-z\-0-9]+)+)(?:\:(\d{1,5}))?))((?:\/+[^\/\?\#]+)+\/{0,})(\?[^#]{0,})?(\#[^#]{0,})$/gi;
-    const [ href, oringin, protocol, host, hostname, port, pathname, search, hash ] = rUrl.exec(urlString) || [];
-    const urlObj = { href, oringin, protocol, host, hostname, port, pathname, search, hash };
+    const [ href, origin, protocol, host, hostname, port, pathname, search, hash ] = rUrl.exec(urlString) || [];
+    const urlObj = { href, origin, protocol, host, hostname, port, pathname, search, hash };
 
     Object.keys((urlObj)).forEach((name) => {
       this[ name ] = urlObj[ name ];
