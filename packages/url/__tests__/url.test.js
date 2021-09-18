d898b4f81c62ee74e1838532e19e97639d9301d3
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-18 19:48:57 +0800

    修正一处正则错误

--- packages/url/__tests__/url.test.js
+++ packages/url/__tests__/url.test.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const url = require('..');
+const path = require('path');
+const Url = require(path.resolve(__dirname, '..')).default;
 
-describe('@trz/url', () => {
-    it('needs tests');
+
+
+describe('@trz/type', () => {
+    console.log(new Url('https://www.google.com/search?q=%E9%98%BF%E6%96%AF%E8%92%82%E8%8A%AC&newwindow=1&sxsrf=AOaemvK8dYw-Gh3OqCY84scQmwkrcg2oBQ%3A1631845261685&ei=jftDYbanKcnA0PEP9L-s-As&oq=%E9%98%BF%E6%96%AF%E8%92%82%E8%8A%AC&gs_lcp=Cgdnd3Mtd2l6EAM6BwgjELADECc6BwgAEEcQsAM6BAgAEEM6BQgAEIAESgQIQRgAUIaGsDlY-omwOWDJkLA5aANwAngAgAHiA4gBug-SAQcyLTEuMi4ymAEAoAEByAEEwAEB&sclient=gws-wiz&ved=0ahUKEwj2oZqp-YTzAhVJIDQIHfQfC78Q4dUDCA4&uact=5'))
+    test('Function: type.is', () => {
+        // expect(type.is([], 'array')).toBe(true);
+    });
 });
