78e28bff7f35884894e1bec31efa5a609169a35b
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-09-22 11:01:49 +0800

    upd: -

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -1,16 +1,37 @@
 
 class Url {
-  constructor(urlString: string) {
+  href: string = '';
+
+  origin: string = '';
+
+  protocol: string = '';
+
+  host: string = '';
+
+  hostname: string = '';
+
+  port: string = '';
+
+  pathname: string = '';
+
+  search: string = '';
+
+  hash: string = '';
+
+  constructor(urlString?: string) {
     const rUrl = /((?:^((?:[a-z]+)\:)?\/\/)?(((?:\.?[a-z\-0-9]+)+)(?:\:(\d{1,5}))?))((?:\/+[^\/\?\#]+)+\/{0,})(\?[^#]{0,})?(\#[^#]{0,})?$/gi;
     const [ href, origin, protocol, host, hostname, port, pathname, search, hash ] = rUrl.exec(urlString) || [];
     const urlObj = { href, origin, protocol, host, hostname, port, pathname, search, hash };
 
     Object.keys((urlObj)).forEach((name) => {
       this[ name ] = urlObj[ name ] || '';
-    })
+    });
+  }
 
+  toString() {
+    return this.href;
   }
 }
 
-export default Url;
 
+export default Url;
