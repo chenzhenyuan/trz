75da925a816d06da6cfb1679ad6c49f9867b7a51
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-10-18 19:19:55 +0800

    -

--- packages/url/src/url.ts
+++ packages/url/src/url.ts
@@ -1,10 +1,16 @@
 
 import Uri from "./Uri";
 
+import * as path from './pathname';
+
 export const parse = (url?: string): any => new Uri(url);
 
 export const stringify = (str?: typeof Uri): string | undefined => str?.toString();
 
-export default {
-  parse, stringify
-};
+export default { parse, stringify };
+
+
+const target = '../a/b/c/d/./../../../C1/C2/../s/./';
+console.group(target);
+console.log(path.normalize(target));
+console.groupEnd();
