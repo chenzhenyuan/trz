4582ebd492bca99ed5b359fbb4db79bbdbe85e7a
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-10-26 19:37:23 +0800

    udp: packages/fetch

--- packages/fetch/src/main.ts
+++ packages/fetch/src/main.ts
@@ -1,3 +1,47 @@
 
+interface FetchInterface {
+  GET(url: string): PromiseLike<any>;
 
-export { default as default, Fetch } from './fetch';
+  GET(url: string, searchParamse: string): PromiseLike<any>;
+}
+
+interface FetchConstructor {
+  new(instanceConfigs?: any): FetchInterface;
+}
+
+
+const fetchCore = ({ url }: any = {}) => {
+  console.log(url);
+
+  return new Promise((resolve, reject) => {
+    resolve({});
+  });
+};
+
+
+// @ts-ignore
+export const Fetch: FetchConstructor = function(...args) {
+  const property: any = {};
+
+  const fetchAgent = () => fetchCore({});
+
+  class Fetch {
+    [ k: string ]: any;
+
+    constructor() {
+      property.n = 'aaa';
+    }
+
+    GET(url: string) {
+      return fetchAgent();
+    }
+  }
+
+
+  return new Fetch();
+};
+
+export default new Fetch();
+
+
+
---
+++ packages/fetch/types/main.d.ts
@@ -0,0 +1,4 @@
+
+declare class Network  {
+  constructor(message: string);
+}
