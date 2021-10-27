6d528ad1175e233fe1882c41678785672857733b
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-10-27 20:11:02 +0800

    -

--- packages/fetch/src/net.ts
+++ packages/fetch/src/net.ts
@@ -1,51 +1,84 @@
+import { fetchCore } from './core';
+import T from '@trz/type'
 
-interface NetInterface {
-  GET(url: string): PromiseLike<any>;
+import { Network as NetConstructor, SearchParamsInterface } from '../index.d';
 
-  GET(url: string, searchParamse: string): PromiseLike<any>;
+
+// const fetchCore = ({ url }: any = {}): PromiseLike<ResponseInterface> => {
+//   return new Promise((resolve, reject) => {
+//     fetch(url, {});
+//   });
+// };
+
+
+/**
+ * @decorator
+ */
+const ResponseAgent = function (...args: any[]) {
 }
 
-interface NetConstructor {
-  new(instanceConfigs?: any): NetInterface;
+
+/**
+ * @decorator
+ */
+const RequestBefore = function (reqConfigs = {}) {
+  return function (ins: any, name: string, descriptor: any) {
+    const _RquestMethod = descriptor.value;
+
+    const req = new Request('aaaa', { "method": name });
+
+    descriptor.value = function (url: string, params?: SearchParamsInterface): typeof _RquestMethod {
+      return _RquestMethod.call(this, req.url, req);
+    }
+  }
 }
-
-
-const netCore = ({ url }: any = {}) => {
-  console.log(url);
-
-  return new Promise((resolve, reject) => {
-    resolve({});
-  });
-};
-
 
 // @ts-ignore
-export const Net: NetConstructor = function(instanceConfigs): NetInterface {
-  const property: any = {};
+export const Network: NetConstructor = function(instanceConfigs) {
+  let properties: any;
 
-  const reqAgent = () => netCore({});
-
-  class Request {
+  class Network {
     [ k: string ]: any;
 
-    get n() {
-      return property.n;
+    get response() {
+      return properties.response;
     }
 
-    constructor(n) {
-      property.n = n;
+    constructor(basicConfigs?: any) {
+      const { pathname } = <any>basicConfigs;
+
+      properties = {};
+
+      if (T.is(pathname, 'string')) {
+        properties.pathname = pathname;
+        Object.defineProperty(this, 'pathname', { value: pathname, enumerable: true, writable: false, configurable: false });
+      }
     }
 
-    GET(url: string) {
-      console.log(property);
-      return reqAgent();
+    // @ResponseAgent
+    @RequestBefore(properties)
+    GET(url: string, opts?: any): PromiseLike<any> {
+      console.log('GET:url', url, opts);
+      return fetchCore(url, opts);
+    }
+
+    @RequestBefore(properties)
+    POST(url: string | unknown, opts?: unknown) {
+      console.log('POST:url', url, opts);
+    }
+
+
+    setResponse(respHandler: () => PromiseLike<any>) {
+      properties.respHandler = respHandler;
     }
   }
 
-  return new Request(instanceConfigs);
+
+  return new Network(instanceConfigs);
 };
 
-export default new Net('aaaaa');
+
+export default new Network('aaaaa');
 
 
 
