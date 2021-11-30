d28487ddd08502af67ff8c7d8cdf06411975fa0a
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2021-11-30 12:44:23 +0800

    upd: 1.add some function; 2.update code;

--- packages/type/src/type.ts
+++ packages/type/src/type.ts
@@ -7,28 +7,32 @@ const types = (any: unknown): string => {
 
 
 export const is = (source: unknown, assert: string | unknown): boolean => {
-  if (types(assert) !== 'string') return false;
+  if (types(assert) !== 'string') {
+    return false;
+  }
 
   return types(source) === String.prototype.toLowerCase.call(assert);
 };
 
-export const some = (source: unknown, ...typeList: string[]): boolean => {
-  return Array.from(typeList).includes(types(source));
-};
+export const some = (source: unknown, assertList: string[]): boolean => (
+  Array.isArray(assertList) && assertList.some((assert: string) => (
+    types(source) === String.prototype.toLowerCase.call(assert)
+  ))
+);
+
+export const isUndefined = (source: unknown): boolean => source === undefined;
+
+export const isNull = (source: unknown): boolean => source === null;
 
 export const isString = (source: unknown): boolean => is(source, 'string');
 
-export const isArray = (source: unknown): boolean => is(source, 'array');
-
-export const isObject = (source: unknown): boolean => is(source, 'object');
-
-export const isNull = (source: unknown): boolean => is(source, 'null');
+export const isNumber = (source: unknown): boolean => is(source, 'number');
 
 export const isBoolean = (source: unknown): boolean => is(source, 'boolean');
 
-export const isNumber = (source: unknown): boolean => is(source, 'number');
+export const isObject = (source: unknown): boolean => is(source, 'object');
 
-export const isUndefined = (source: unknown): boolean => is(source, 'undefined');
+export const isArray = (source: unknown): boolean => is(source, 'array');
 
 export const of = types;
 
