e92d75b55aa01cae0055320fcce63d278e7d3418
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2022-01-19 16:49:33 +0800

    upd: 修复 @trz/uri 部分问题

--- development/index.ts
+++ development/index.ts
@@ -3,7 +3,7 @@
  * @since        : 2021/10/20 12:38:12 +0800
  * @filePath     : /development/index.ts
  * @lastEditors  : JAYNE·CHEN
- * @updated      : 2022/01/19 15:03:38 +0800
+ * @updated      : 2022/01/19 16:12:17 +0800
  * @description  : 调试模块入口文件
  */
 
@@ -20,48 +20,5 @@
 // console.log('path.resolve() ::', pathname.resolve('./bar', '../111', './baz'));
 // console.groupEnd();
 
-import "./@trz-type"
-
-
-
-// import Uri, { SearchParams } from '@trz/uri';
-// // console.log(new Uri());
-// new Uri('//asdf@120.356.2.44///demo/pathname/filename.ext?searchKey=1&searchString=2#hashKey=1&hashString=2');
-
-// console.log(new Uri('?sKey=值').searchParams + '');
-// console.log(new Uri('./pathname'));
-// console.log(new Uri('file:///pathname?sss=11'));
-// console.log(new Uri('file:///pathname#sss=11'));
-// console.log(new Uri('file:///pathname?sss=11#hhh=2222'));
-
-// const querySearch = new SearchParams('?sss=111&sss=2222');
-// querySearch.set('aa', 'aaa');
-// querySearch.sort();
-// console.log(querySearch, querySearch.toString());
-
-// const hashString = new HashParams('#sss=111&sss=2222');
-// hashString.set('aa', 'aaa');
-// hashString.sort();
-// console.log(hashString, hashString.toString());
-
-// const uri = new Uri(location.href);
-
-// console.log('before::', uri);
-// console.log('before::', uri.host);
-// uri.host = 123;
-// console.log('after::', uri);
-// console.log('after::', uri.host);
-// uri.host = '789';
-// console.log('after::', uri);
-// console.log('after::', uri.host);
-
-
-// const uri = new Uri('./prefix/user/', '../UserId');
-
-// console.log(uri);
-// uri.appendSearch('sss', {a: 1});
-// uri.appendSearch({ 'mmm': '23412' });
-// uri.appendSearch([[ 'a', 2 ], [ 'a', 3 ]]);
-
-// // uri.appendSearch({ aa: 'aa' });
-// console.log(decodeURIComponent(uri.searchParams + ''));
+// import "./type";
+import "./uri";
--- packages/uri/src/uri.ts
+++ packages/uri/src/uri.ts
@@ -1,12 +1,13 @@
 
 /* eslint-disable no-empty-function */
 /* eslint-disable @typescript-eslint/no-empty-function */
-import type from '@trz/type';
+
 import Serialize from '@trz/serialize';
+import T from '@trz/type';
+
 
 class ParamsError extends Error {
   name = 'ParamsError';
-
   constructor(message: string) {
     super(message);
 
@@ -18,12 +19,12 @@ class ParamsError extends Error {
   }
 }
 
+
 export class SearchParams extends Serialize {
   constructor(search = '') {
-    if (search.slice(0, 1) != '?') {
-      throw new ParamsError('The params must be a search string.');
-    }
-
+    // if (search.slice(0, 1) != '?') {
+    //   throw new ParamsError('The params must be a search string.');
+    // }
     super((typeof search === 'string' ? search : '').replace(/#.*$/i, '').replace(/^\?/i, ''));
   }
 
@@ -36,10 +37,9 @@ export class SearchParams extends Serialize {
 
 export class HashParams extends Serialize {
   constructor(hash = '') {
-    if (hash.slice(0, 1) != '#') {
-      throw new ParamsError('The params must be a hash string.');
-    }
-
+    // if (hash.slice(0, 1) != '#') {
+    //   throw new ParamsError('The params must be a hash string.');
+    // }
     super((typeof hash === 'string' ? hash : '').replace(/\?[^#]*/i, '').replace(/^#/i, ''));
   }
 
@@ -67,54 +67,54 @@ export class Uri extends URL {
   constructor(...args: any[]) {
     super(mergeUri(...args));
   }
-
   setSearch(name: any, value?: any) {
     let params = [];
 
-    if (type.is(name, 'string')) {
+    if (T.is(name, 'string') && name.length) {
       params = [[ name, value ]];
     }
-    else if (type.is(name, 'object')) {
+    else if (T.is(name, 'object')) {
       params = Object.entries(name);
     }
-    else if (type.is(name, 'array')) {
+    else if (T.is(name, 'array')) {
       params = <[string, any]>name;
     }
     else {
       throw TypeError('参数类型错误，请使用正确的参数。');
     }
 
-    params.forEach(([ k, v ]) => {
-      console.log([ k, v ]);
-      this.searchParams.set(k, JSON.stringify(v));
+    params.forEach(([ key, val ]) => {
+      const v = val ?? '';
+      this.searchParams.set(key, T.is(v, 'string') ? v: JSON.stringify(v));
     });
   }
-
   appendSearch(name: any, value?: any) {
     let params: string[][] = [];
 
-    if (type.is(name, 'string')) {
+    if (T.is(name, 'string') && name.length) {
       params = [[ name, value ]];
     }
-    else if (type.is(name, 'object')) {
+    else if (T.is(name, 'object')) {
       params = Object.entries(name);
     }
-    else if (type.is(name, 'array')) {
+    else if (T.is(name, 'array')) {
       params = <[string, any]>name;
     }
     else {
       throw TypeError('参数类型错误，请使用正确的参数。');
     }
 
-    params.forEach(([ k, v ]) => {
-      this.searchParams.append(k, JSON.stringify(v));
+    params.forEach(([ key, val ]) => {
+      const v = val ?? '';
+      this.searchParams.append(key, T.is(v, 'string') ? v: JSON.stringify(v));
     });
   }
-
   removeSearch(...names: string[]) {
   }
 }
 
+
 Object.defineProperty(Uri.prototype, Symbol.toStringTag, { value: 'Uri' });
+
 
 export default Uri;
