8e0790e4b1d7f0a7fab1fcb73d44773e04cc1335
ZHENYUAN <JAYNE@CHENZHENYUAN.COM>
2022-04-20 02:04:22 +0800

    fixed a bug

--- packages/hooks/src/useQueries/index.ts
+++ packages/hooks/src/useQueries/index.ts
@@ -3,7 +3,7 @@
  * @since        : 2021/12/20 13:13:03 +0800
  * @filePath     : /packages/hooks/src/useQueries/index.ts
  * @lastEditors  : JAYNE·CHEN
- * @updated      : 2022/04/20 00:38:37 +0800
+ * @updated      : 2022/04/20 02:03:33 +0800
  * @description  : ****
  */
 
@@ -12,16 +12,18 @@ import { useEffect, useLayoutEffect, useState, useCallback } from 'react';
 import { useLocation, useHistory } from 'react-router';
 
 
-type SetQueriesAction<Q> = ((prevQueries: Q) => Q);
+export type InitialQueries<V> = V | (() => V);
 
-type DispatchQueries<A> = (queries: A) => void;
+export type SetQueriesAction<Q> = ((prevQueries: Q) => Q);
 
-type NavigatorAction<S = string> = (search: S) => void;
+export type DispatchQueries<A> = (queries: A) => void;
 
-type InitialQueries<V> = V | (() => V);
+export type NavigatorAction<S = string> = (search: S) => void;
 
-export function toSearchString<M>(queriesObj: M): string {
-  const target: string[] = Object.entries(queriesObj ?? {}).map(([ k, v ]: any[]) => {
+
+/* -------------------------------------------------------------------------- */
+export function toSearchString<C>(queries: C): string {
+  const target: string[] = Object.entries(queries ?? {}).map(([ k, v ]: any[]) => {
     if ([ null, undefined, '' ].includes(v)
         || (Array.isArray(v) && !v.length)
           || (typeof v === 'object' && !Object.keys(v).length) ) {
@@ -38,7 +40,9 @@ export function toSearchString<M>(queriesObj: M): string {
   return target.filter((v) => v !== '').join('&');
 }
 
-export function toQueriesObject<P>(search: string): any {
+
+/* -------------------------------------------------------------------------- */
+export function toQueriesObject<search = string>(search: string): any {
   const searchEntries = Array.from((new URLSearchParams(search) as any).entries());
 
   return Object.fromEntries(searchEntries.map(([ k, v ]: any) => {
@@ -62,47 +66,67 @@ export function toQueriesObject<P>(search: string): any {
   }));
 }
 
-export function useQueriesCore<Q>(initialQueries?: InitialQueries<Q>, navi?: NavigatorAction): [Q, DispatchQueries<Q | SetQueriesAction<Q>>] {
-  const location = useLocation();
 
-  const currentSearch = location.search.replace(/^\?/, '');
-  const [ pathname ] = useState<string>(location.pathname);
-  const [ queries, setQueries ] = useState<Q>(currentSearch === '' ?( initialQueries ?? {}) as any : toQueriesObject<Q>(currentSearch));
+/* -------------------------------------------------------------------------- */
+/**
+ * @param   {InitialQueries}  initialQueries
+ * @param   {NavigatorAction} navigator
+ * @return  {*}
+ */
+export function useQueriesCore<Q>(initialQueries?: InitialQueries<Q>, navigator?: NavigatorAction): [Q, DispatchQueries<Q | SetQueriesAction<Q>>] {
+  const routerLocation = useLocation();
+  const current = routerLocation.search.replace(/^\?/, '');
+
+  const [ pathname ] = useState<string>(routerLocation.pathname);
+  const [ queries, setQueries ] = useState<Q>((): Q => {
+    if (current === '') {
+      return toQueriesObject(toSearchString(initialQueries));
+    }
+    return toQueriesObject(current);
+  });
   // --------
   const dispatch = useCallback((nextQueries: Q | SetQueriesAction<Q>): void => {
     setQueries((searches: Q): Q => {
-      if (typeof nextQueries === 'function') {
-        return toQueriesObject(toSearchString((nextQueries as SetQueriesAction<Q>)(searches)));
-      }
-
-      return toQueriesObject(toSearchString(nextQueries as Q));
+      return toQueriesObject(toSearchString(
+        typeof nextQueries === 'function'
+          ? (nextQueries as SetQueriesAction<Q>)(searches)
+          : nextQueries as Q
+      ));
     });
   }, [ queries ]);
 
   useEffect(() => {
-    navi && navi('?' + toSearchString(queries ?? {}));
+    if (pathname !== routerLocation.pathname) return;
+
+    if (current === '') {
+      navigator && navigator(`?${toSearchString(initialQueries)}`);
+      return;
+    }
+
+    setQueries((queries) => (
+      current === toSearchString(queries)
+        ? queries
+        : toQueriesObject(current)
+    ));
+  }, [ current, pathname, routerLocation.pathname ]);
+
+  useEffect(() => {
+    navigator && navigator(`?${toSearchString(queries)}`);
   }, [ queries ]);
-
-  useLayoutEffect(() => {
-    if (pathname !== location.pathname || currentSearch === '') return;
-
-    setQueries((queries) => {
-      if (currentSearch === toSearchString(queries ?? {})) return queries;
-
-      return toQueriesObject(currentSearch);
-    });
-  }, [ currentSearch, pathname, location.pathname ]);
 
   return [ queries, dispatch ];
 }
 
+/* -------------------------------------------------------------------------- */
+/*
 export function useQueries<Q>(initialQueries?: InitialQueries<Q>): [Q, DispatchQueries<Q | SetQueriesAction<Q>>] {
   const syncQueries = useHistory();
   const [ queries, setQueries ] = useQueriesCore<Q>(initialQueries, (search) => {
     syncQueries.replace(search);
   });
-
   return [ queries, setQueries ];
 }
+*/
 
-export default useQueries;
+/* -------------------------------------------------------------------------- */
+export default useQueriesCore;
